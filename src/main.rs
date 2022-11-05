e4fa4232b0d4961454ed708ea093bdfc907ba5d6
Fumiya Fujinaka <uzuna.kf@gmail.com>
2022-11-05 20:23:04 +0900

    add run command on gstreamer-rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use derive_more::{Display, Error};
+use gst::{
+    prelude::GstBinExtManual,
+    traits::{ElementExt, GstObjectExt},
+};
 use std::process::Command;
 use structopt::StructOpt;
 
@@ -10,6 +15,60 @@ enum Gst {
     },
     /// run gstremer with gstreamer-rs
     Run,
+}
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
+struct ErrorMessage {
+    src: String,
+    error: String,
+    debug: Option<String>,
+    source: gst::glib::Error,
+}
+
+/// run gstreamer
+fn gst_main_loop() -> Result<(), anyhow::Error> {
+    gst::init()?;
+
+    // setup element
+    let pipeline = gst::Pipeline::new(None);
+    let src = gst::ElementFactory::make("videotestsrc").build()?;
+    let sink = gst::ElementFactory::make("autovideosink").build()?;
+
+    // add to pipeline and link elements
+    pipeline.add_many(&[&src, &sink])?;
+    gst::Element::link_many(&[&src, &sink])?;
+
+    // start playing
+    pipeline.set_state(gst::State::Playing)?;
+    let bus = pipeline
+        .bus()
+        .expect("Pipeline without bus. Shouldn't happen!");
+
+    for msg in bus.iter_timed(gst::ClockTime::NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                pipeline.set_state(gst::State::Null)?;
+                return Err(ErrorMessage {
+                    src: msg
+                        .src()
+                        .map(|s| String::from(s.path_string()))
+                        .unwrap_or_else(|| String::from("None")),
+                    error: err.error().to_string(),
+                    debug: err.debug(),
+                    source: err.error(),
+                }
+                .into());
+            }
+            _ => (),
+        }
+    }
+
+    pipeline.set_state(gst::State::Null)?;
+    Ok(())
 }
 
 fn main() {
@@ -36,6 +95,10 @@ fn main() {
                 }
             }
         }
-        Gst::Run => todo!(),
+        Gst::Run => {
+            if let Err(e) = gst_main_loop() {
+                log::error!("gstream error: {:?}", e);
+            }
+        }
     }
 }
