039b77c4af35f8ba47ec7e3d86d99f2386fd17b4
Fumiya Fujinaka <uzuna.kf@gmail.com>
2022-11-08 21:24:30 +0900

    base ident

--- plugin/src/testtrans/imp.rs
+++ plugin/src/testtrans/imp.rs
@@ -1,6 +1,8 @@
 use gst::glib;
+use gst::prelude::ElementClassExt;
+use gst::prelude::PadExtManual;
 use gst::subclass::prelude::*;
-
+use gst::traits::ElementExt;
 use once_cell::sync::Lazy;
 
 use super::CLASS_NAME;
@@ -14,8 +16,37 @@ static CAT: Lazy<gst::DebugCategory> = Lazy::new(|| {
     )
 });
 
-#[derive(Debug, Default)]
-pub struct TestTrans {}
+pub struct TestTrans {
+    srcpad: gst::Pad,
+    sinkpad: gst::Pad,
+}
+
+impl TestTrans {
+    fn sink_chain(
+        &self,
+        pad: &gst::Pad,
+        buffer: gst::Buffer,
+    ) -> Result<gst::FlowSuccess, gst::FlowError> {
+        gst::log!(CAT, obj: pad, "Handling buffer {:?}", buffer);
+        self.srcpad.push(buffer)
+    }
+    fn sink_event(&self, pad: &gst::Pad, event: gst::Event) -> bool {
+        gst::log!(CAT, obj: pad, "Handling event {:?}", event);
+        self.srcpad.push_event(event)
+    }
+    fn sink_query(&self, pad: &gst::Pad, query: &mut gst::QueryRef) -> bool {
+        gst::log!(CAT, obj: pad, "Handling query {:?}", query);
+        self.srcpad.peer_query(query)
+    }
+    fn src_event(&self, pad: &gst::Pad, event: gst::Event) -> bool {
+        gst::log!(CAT, obj: pad, "Handling event {:?}", event);
+        self.sinkpad.push_event(event)
+    }
+    fn src_query(&self, pad: &gst::Pad, query: &mut gst::QueryRef) -> bool {
+        gst::log!(CAT, obj: pad, "Handling query {:?}", query);
+        self.sinkpad.peer_query(query)
+    }
+}
 
 impl ElementImpl for TestTrans {
     // エレメントの仕様について記述する
@@ -71,7 +102,14 @@ impl ElementImpl for TestTrans {
     }
 }
 
-impl ObjectImpl for TestTrans {}
+impl ObjectImpl for TestTrans {
+    fn constructed(&self) {
+        self.parent_constructed();
+        let obj = self.obj();
+        obj.add_pad(&self.sinkpad).unwrap();
+        obj.add_pad(&self.srcpad).unwrap();
+    }
+}
 
 impl GstObjectImpl for TestTrans {}
 
@@ -80,4 +118,50 @@ impl ObjectSubclass for TestTrans {
     const NAME: &'static str = CLASS_NAME;
     type Type = super::TestTrans;
     type ParentType = gst::Element;
+
+    fn with_class(klass: &Self::Class) -> Self {
+        let templ = klass.pad_template("sink").unwrap();
+        let sinkpad = gst::Pad::builder_with_template(&templ, Some("sink"))
+            .chain_function(|pad, parent, buffer| {
+                TestTrans::catch_panic_pad_function(
+                    parent,
+                    || Err(gst::FlowError::Error),
+                    |identity| identity.sink_chain(pad, buffer),
+                )
+            })
+            .event_function(|pad, parent, event| {
+                TestTrans::catch_panic_pad_function(
+                    parent,
+                    || false,
+                    |identity| identity.sink_event(pad, event),
+                )
+            })
+            .query_function(|pad, parent, query| {
+                TestTrans::catch_panic_pad_function(
+                    parent,
+                    || false,
+                    |identity| identity.sink_query(pad, query),
+                )
+            })
+            .build();
+
+        let templ = klass.pad_template("src").unwrap();
+        let srcpad = gst::Pad::builder_with_template(&templ, Some("src"))
+            .event_function(|pad, parent, event| {
+                TestTrans::catch_panic_pad_function(
+                    parent,
+                    || false,
+                    |identity| identity.src_event(pad, event),
+                )
+            })
+            .query_function(|pad, parent, query| {
+                TestTrans::catch_panic_pad_function(
+                    parent,
+                    || false,
+                    |identity| identity.src_query(pad, query),
+                )
+            })
+            .build();
+        Self { srcpad, sinkpad }
+    }
 }
