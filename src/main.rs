37196904fec8b800b083ee62334af326450498ef
Fumiya Fujinaka <uzuna.kf@gmail.com>
2022-11-08 11:01:18 +0900

    add caps filter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use derive_more::{Display, Error};
 use gst::{
-    prelude::{GObjectExtManualGst, GstBinExtManual},
+    prelude::{ElementExtManual, GObjectExtManualGst, GstBinExtManual},
     traits::{ElementExt, GstObjectExt},
     Element, Pipeline,
 };
@@ -18,16 +18,22 @@ enum Gst {
         print: bool,
         #[structopt(flatten)]
         testsrc: VideoTestSrcOpt,
+        #[structopt(flatten)]
+        videocaps: VideoCapsOpt,
     },
     /// run gstremer with gstreamer-rs
     Run {
         #[structopt(flatten)]
         testsrc: VideoTestSrcOpt,
+        #[structopt(flatten)]
+        videocaps: VideoCapsOpt,
     },
     /// use appsrc and textoverlay
     AppSrcText {
         #[structopt(flatten)]
         testsrc: VideoTestSrcOpt,
+        #[structopt(flatten)]
+        videocaps: VideoCapsOpt,
     },
 }
 
@@ -66,6 +72,48 @@ impl Default for VideoTestSrcPattern {
         Self::Smpte
     }
 }
+
+#[derive(Debug, StructOpt)]
+struct VideoCapsOpt {
+    // 画面の横幅
+    #[structopt(default_value = "600", long)]
+    width: i32,
+    // 画面の高さ
+    #[structopt(default_value = "400", long)]
+    height: i32,
+    // フレームレート(整数のみ)
+    #[structopt(default_value = "30", long)]
+    fps: i32,
+}
+
+impl Default for VideoCapsOpt {
+    fn default() -> Self {
+        Self {
+            width: 600,
+            height: 400,
+            fps: 30,
+        }
+    }
+}
+
+impl VideoCapsOpt {
+    fn write_property_string(&self, v: &mut Vec<String>) {
+        v.push(format!(
+            "video/x-raw,width={},height={},framerate={}/1",
+            self.width, self.height, self.fps
+        ));
+    }
+    fn get_caps(&self) -> gst::Caps {
+        // gst_video::Caps::VideoCapsBuilderを使うともっと直感的に書ける
+        // i32なのはC実装が受け付ける型がi32であるため他の型ではエラーになる
+        gst::Caps::builder("video/x-raw")
+            .field("width", self.width)
+            .field("height", self.height)
+            .field("framerate", gst::Fraction::new(self.fps, 1))
+            .build()
+    }
+}
+
 #[derive(Debug, Display, Error)]
 #[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
 struct ErrorMessage {
@@ -76,7 +124,10 @@ struct ErrorMessage {
 }
 
 /// run gstreamer
-fn build_gst_run(testsrc: &VideoTestSrcOpt) -> Result<Pipeline, anyhow::Error> {
+fn build_gst_run(
+    testsrc: &VideoTestSrcOpt,
+    videocaps: &VideoCapsOpt,
+) -> Result<Pipeline, anyhow::Error> {
     gst::init()?;
 
     // setup element
@@ -87,7 +138,8 @@ fn build_gst_run(testsrc: &VideoTestSrcOpt) -> Result<Pipeline, anyhow::Error> {
 
     // add to pipeline and link elements
     pipeline.add_many(&[&src, &sink])?;
-    gst::Element::link_many(&[&src, &sink])?;
+    // attach caps
+    src.link_filtered(&sink, &videocaps.get_caps())?;
     Ok(pipeline)
 }
 
@@ -133,9 +185,15 @@ fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     match opt {
-        Gst::Launch { print, testsrc } => {
+        Gst::Launch {
+            print,
+            testsrc,
+            videocaps,
+        } => {
             let mut args: Vec<String> = vec!["videotestsrc".to_string()];
             testsrc.write_property_string(&mut args);
+            args.push("!".to_string());
+            videocaps.write_property_string(&mut args);
             args.push("!".to_string());
             args.push("autovideosink".to_string());
             if print {
@@ -155,15 +213,16 @@ fn main() {
                 }
             }
         }
-        Gst::Run { testsrc } => {
-            let pipeline = build_gst_run(&testsrc).expect("failed to build gst run pipeline");
+        Gst::Run { testsrc, videocaps } => {
+            let pipeline =
+                build_gst_run(&testsrc, &videocaps).expect("failed to build gst run pipeline");
             if let Err(e) = run_pipeline(pipeline) {
                 log::error!("gstream error: {:?}", e);
             }
         }
-        Gst::AppSrcText { testsrc } => {
-            let pipeline =
-                app::build_appsrc_text_overlay(&testsrc).expect("failed to build gst run pipeline");
+        Gst::AppSrcText { testsrc, videocaps } => {
+            let pipeline = app::build_appsrc_text_overlay(&testsrc, &videocaps)
+                .expect("failed to build gst run pipeline");
             if let Err(e) = run_pipeline(pipeline) {
                 log::error!("gstream error: {:?}", e);
             }
