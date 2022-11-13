358b764ea47a1dbf31d7ed39c302470cc122c18a
Fumiya Fujinaka <uzuna.kf@gmail.com>
2022-11-13 23:41:58 +0900

    add deb make

--- common.mk
+++ common.mk
@@ -1,13 +1,16 @@
 # 共通の環境変数設定
 mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 PROJECT_DIR := $(dir $(mkfile_path))
+DEB_DIR := ${PROJECT_DIR}deb
 
-TARGET:=  # Rustのビルドターゲット {<empty>(dev), production}
+# Rustのビルドターゲット {<empty>(dev), production}
+TARGET:=
 
 # ビルドフラグとGstreamer実行の参照先の切り替え
 ifeq (${TARGET}, release)
 	BUILD_FLAG=--release
-	OUT_DIR=${PROJECT_DIR}target/release # 出力先をrustのビルドディレクトリとする
+# 出力先をrustのビルドディレクトリとする
+	OUT_DIR=${PROJECT_DIR}target/release
 else
 	BUILD_FLAG=
 	OUT_DIR=${PROJECT_DIR}target/debug
